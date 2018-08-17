3ee68c72d124ee055c0711dfc5c0a60a2eaf5edf
akimoto2018 <akimoto@mis-se.co.jp>
2018-08-17 10:43:00 +0900

    1st cimmit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: '宮崎駿' },
+  { id: 12, name: '夏目漱石' },
+  { id: 13, name: '宮澤賢治' },
+  { id: 14, name: '星新一' },
+  { id: 15, name: '松尾芭蕉' },
+  { id: 16, name: '石川啄木' },
+  { id: 17, name: '野口英世' },
+  { id: 18, name: '新渡戸稲造' },
+  { id: 19, name: '南方熊楠' },
+  { id: 20, name: '柳田國男' }
+];
